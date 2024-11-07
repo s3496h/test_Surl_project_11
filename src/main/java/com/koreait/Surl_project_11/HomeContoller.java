cc4469b570f8368fb93edabf967ea5646a319216
s3496h <s3496h@gmail.com>
2024-11-07 10:43:04 +0900

    개발환경과 운영환경의 설정파일 분리

--- src/main/java/com/koreait/Surl_project_11/HomeContoller.java
+++ src/main/java/com/koreait/Surl_project_11/HomeContoller.java
@@ -1,13 +1,17 @@
 package com.koreait.Surl_project_11;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+
 @Controller
 public class HomeContoller {
+    @Value("${custom.site.name}")
+    private String customSiteName;
     @GetMapping("/")
     @ResponseBody
-    public String ShowMain(){
-        return "Main";
+    public String ShowMain() {
+        return "Main on " + customSiteName;
     }
 }
