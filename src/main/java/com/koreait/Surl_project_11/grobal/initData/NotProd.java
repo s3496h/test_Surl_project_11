210fc6e83c56cb96e589c4cd00927351a7074d30
s3496h <s3496h@gmail.com>
2024-11-08 15:39:44 +0900

    서비스에 @Transactional 적용

--- src/main/java/com/koreait/Surl_project_11/grobal/initData/NotProd.java
+++ src/main/java/com/koreait/Surl_project_11/grobal/initData/NotProd.java
@@ -4,8 +4,6 @@ import com.koreait.Surl_project_11.domain.article.article.entity.Article;
 import com.koreait.Surl_project_11.domain.article.article.service.ArticleService;
 import com.koreait.Surl_project_11.domain.member.entity.Member;
 import com.koreait.Surl_project_11.domain.member.service.MemberService;
-import com.koreait.Surl_project_11.grobal.eceptions.GlobalException;
-import com.koreait.Surl_project_11.grobal.rsData.RsData;
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -48,12 +46,6 @@ public class NotProd {
 
         Member member1 = memberService.join("user1", "1234", "유저 1").getData();
         Member member2 = memberService.join("user2", "1234", "유저 2").getData();
-        try {
-            RsData<Member> joinRs = memberService.join("user2", "1234", "유저 2");
-        } catch (GlobalException e) {
-            System.out.println("e.getMsg() : " + e.getRsData().getMsg());
-            System.out.println("e.getStatusCode() : " + e.getRsData().getStatusCode());
-        }
 
         Article article1 = articleService.write("제목 1", "내용 1").getData();
         Article article2 = articleService.write("제목 2", "내용 2").getData();
