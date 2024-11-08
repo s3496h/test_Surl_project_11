6780e1ef21de0498769684236a8c4a3047d3a3aa
s3496h <s3496h@gmail.com>
2024-11-08 13:13:49 +0900

    엔티티의 생성,수정날짜 자동기입

--- src/main/java/com/koreait/Surl_project_11/TestSurlProject11Application.java
+++ src/main/java/com/koreait/Surl_project_11/TestSurlProject11Application.java
@@ -2,8 +2,10 @@ package com.koreait.Surl_project_11;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class TestSurlProject11Application {
 
     public static void main(String[] args) {
--- src/main/java/com/koreait/Surl_project_11/grobal/initData/NotProd.java
+++ src/main/java/com/koreait/Surl_project_11/grobal/initData/NotProd.java
@@ -13,6 +13,7 @@ import org.springframework.context.annotation.Profile;
 
 import java.util.List;
 import java.util.Optional;
+
 // !prod == dev or test
 @Profile("!prod")
 @Configuration
@@ -25,6 +26,7 @@ public class NotProd {
     // 외부객에체 의한 메서드 호출은 @Transactional이 작동해
     // @Lazy, @Autowired 조합은 this의 외부 호출 모드 버전 self를 얻을 수 있다.
     // self를 통한 메서드 호출은 @Transactional을 작동 시킬 수 있어
+
     private final ArticleRepository articleRepository;
 
     @Bean // 개발자가 new 하지 않아도 스프링부트가 직접 관리하는 객체
@@ -41,26 +43,33 @@ public class NotProd {
 
         //articleRepository.deleteAll();
 
-        Article article1 = Article.builder().
-                title("제목1")
+        Article article1 = Article.builder()
+                .title("제목1")
                 .body("내용1").build();
 
-        Article article2 = Article.builder().
-                title("제목2")
+        Article article2 = Article.builder()
+                .title("제목2")
                 .body("내용2").build();
 
 
         articleRepository.save(article1);
         articleRepository.save(article2);
+
         article2.setTitle("제목 2-2");
+
         articleRepository.delete(article1);
     }
+
     @Transactional
     public void work2() {
         // List : 0 ~ N
         // Optional : 0 ~ 1
         Optional<Article> opArticle = articleRepository.findById(2L); // JpaRepository 기본 제공
+
         List<Article> articles = articleRepository.findAll(); // JpaRepository 기본 제공
 
+        List<Article> articlesByIn = articleRepository.findByIdInOrderByTitleDescIdAsc(List.of(1L, 2L));
+        articleRepository.findByTitleContaining("제목");
+        articleRepository.findByTitleAndBody("제목", "내용");
     }
 }
