27d3f5f78b1ddb36a8c08ce25dac2e760e4f27cc
s3496h <s3496h@gmail.com>
2024-11-08 15:19:17 +0900

    오류상태에서 리턴이 아닌 예외발생

--- src/main/java/com/koreait/Surl_project_11/domain/member/service/MemberService.java
+++ src/main/java/com/koreait/Surl_project_11/domain/member/service/MemberService.java
@@ -2,20 +2,26 @@ package com.koreait.Surl_project_11.domain.member.service;
 
 import com.koreait.Surl_project_11.domain.member.entity.Member;
 import com.koreait.Surl_project_11.domain.member.repository.MemberRepository;
+import com.koreait.Surl_project_11.grobal.eceptions.GlobalException;
 import com.koreait.Surl_project_11.grobal.rsData.RsData;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.util.Optional;
+
 @Service
 @RequiredArgsConstructor
 public class MemberService {
     private final MemberRepository memberRepository;
+
     public RsData<Member> join(String username, String password, String nickname) {
-        boolean present = findByUsername(username).isPresent();
-        if (present) {
-            return RsData.of("400-1", "이미 존재하는 아이디입니다.", Member.builder().build());
-        }
+        //boolean present = findByUsername(username).isPresent();
+        //if (present) {
+        //    throw new GlobalException("400-1", "이미 존재하는 아이디야");
+        //}
+        findByUsername(username).ifPresent(ignored -> {
+            throw new GlobalException("400-1", "이미 존재하는 아이디야");
+        });
         Member member = Member.builder()
                 .username(username)
                 .password(password)
@@ -24,6 +30,7 @@ public class MemberService {
         memberRepository.save(member);
         return RsData.of("회원가입이 완료되었습니다.", member);
     }
+
     private Optional<Member> findByUsername(String username) {
         return memberRepository.findByUsername(username);
     }
