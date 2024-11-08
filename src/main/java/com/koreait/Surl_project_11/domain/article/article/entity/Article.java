c2af4ab95ecbd3d019c2e3fe11d492cb6704ec53
s3496h <s3496h@gmail.com>
2024-11-08 15:45:16 +0900

    MappedSuperClass

--- src/main/java/com/koreait/Surl_project_11/domain/article/article/entity/Article.java
+++ src/main/java/com/koreait/Surl_project_11/domain/article/article/entity/Article.java
@@ -1,32 +1,23 @@
 package com.koreait.Surl_project_11.domain.article.article.entity;
 
-import jakarta.persistence.*;
+import com.koreait.Surl_project_11.grobal.jpa.entity.BaseTime;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EntityListeners;
 import lombok.*;
-import org.springframework.data.annotation.CreatedDate;
-import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
-import java.time.LocalDateTime;
-
-import static jakarta.persistence.GenerationType.IDENTITY;
+import static lombok.AccessLevel.PROTECTED;
 
 @Entity
 @Builder
 @Getter
 @Setter
-@NoArgsConstructor
-@AllArgsConstructor
+@NoArgsConstructor(access = PROTECTED)
+@AllArgsConstructor(access = PROTECTED)
 @EntityListeners(AuditingEntityListener.class)
-public class Article {
-    @Id
-    @GeneratedValue(strategy = IDENTITY)
-    private long id;
-    @CreatedDate
-    private LocalDateTime createDate;
-    @LastModifiedDate
-    private LocalDateTime modifyDate;
+public class Article extends BaseTime {
     private String title;
     @Column(columnDefinition = "TEXT")
     private String body;
 }
-
