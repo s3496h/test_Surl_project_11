9cf8e87d86aca915aed5360146fc58796ac8db45
s3496h <s3496h@gmail.com>
2024-11-08 11:47:04 +0900

    게시글 SELECT

--- src/main/java/com/koreait/Surl_project_11/domain/article/article/repository/ArticleRepository.java
+++ src/main/java/com/koreait/Surl_project_11/domain/article/article/repository/ArticleRepository.java
@@ -2,5 +2,6 @@ package com.koreait.Surl_project_11.domain.article.article.repository;
 
 import com.koreait.Surl_project_11.domain.article.article.entity.Article;
 import org.springframework.data.jpa.repository.JpaRepository;
+
 public interface ArticleRepository extends JpaRepository<Article, Long> {
-}
+ }
