acd4de9d31603a14d12ff5ac64f51bea50269535
s3496h <s3496h@gmail.com>
2024-11-08 11:14:56 +0900

    게시글 UPDATE

--- src/main/java/com/koreait/Surl_project_11/grobal/initData/NotProd.java
+++ src/main/java/com/koreait/Surl_project_11/grobal/initData/NotProd.java
@@ -3,10 +3,12 @@ package com.koreait.Surl_project_11.grobal.initData;
 import com.koreait.Surl_project_11.domain.article.article.entity.Article;
 import com.koreait.Surl_project_11.domain.article.article.repository.ArticleRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
+import org.springframework.transaction.annotation.Transactional;
 
 // !prod == dev or test
 @Profile("!prod")
@@ -17,18 +19,30 @@ public class NotProd {
 
     @Bean // 개발자가 new 하지 않아도 스프링부트가 직접 관리하는 객체
     public ApplicationRunner initNotProd() {
-        return args -> {
+        return new ApplicationRunner(){
+            @Override
+            @Transactional
+            public void run(ApplicationArguments args) throws Exception {
+                if (articleRepository.count() > 0) return;
 
-            if (articleRepository.count() > 0) return;
-            //articleRepository.deleteAll();
-            Article article1 = Article.builder().
-                    title("제목1")
-                    .body("내용1").build();
-            Article article2 = Article.builder().
-                    title("제목2")
-                    .body("내용2").build();
-            articleRepository.save(article1);
-            articleRepository.save(article2);
+                //articleRepository.deleteAll();
+
+                Article article1 = Article.builder().
+                        title("제목1")
+                        .body("내용1").build();
+
+                Article article2 = Article.builder().
+                        title("제목2")
+                        .body("내용2").build();
+
+
+                articleRepository.save(article1);
+                articleRepository.save(article2);
+
+                article2.setTitle("제목 2-2");
+
+                articleRepository.delete(article1);
+            }
         };
     }
 }
