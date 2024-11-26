2324b5bc35306cfcb226b40dc8bbfde964904895
s3496h <s3496h@gmail.com>
2024-11-26 15:55:06 +0900

    CustomAuthenticationFilter 추가

--- src/main/java/com/koreait/Surl_project_11/grobal/security/SecurityConfig.java
+++ src/main/java/com/koreait/Surl_project_11/grobal/security/SecurityConfig.java
@@ -1,11 +1,20 @@
 package com.koreait.Surl_project_11.grobal.security;
 
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
 @Configuration
+@RequiredArgsConstructor
+@Slf4j
 public class SecurityConfig {
+
+    private final CustomAuthenticationFilter customAuthenticationFilter;
+
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
@@ -28,7 +37,8 @@ public class SecurityConfig {
                 ) // 타임리프, MPA에서는 csrf를 사용함 // REST API 방식에서는 끈다
                 .formLogin(formLogin ->
                         formLogin.permitAll()
-                );
+                )
+                .addFilterBefore(customAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
         return http.build();
     }
 }
