992df6bc62a6e1e2b15b0ababed6d098c398880a
s3496h <s3496h@gmail.com>
2024-11-26 15:25:00 +0900

    CSRF 끄기

---
+++ src/main/java/com/koreait/Surl_project_11/grobal/security/SecurityConfig.java
@@ -0,0 +1,34 @@
+package com.koreait.Surl_project_11.grobal.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+@Configuration
+public class SecurityConfig {
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeRequests(authorizeRequests ->
+                        authorizeRequests
+                                .requestMatchers("/h2-console/**").permitAll()
+                                .requestMatchers("/actuator/**").permitAll()
+                                .anyRequest().authenticated()
+                )
+                .headers(
+                        headers ->
+                                headers.frameOptions(
+                                        frameOptions ->
+                                                frameOptions.sameOrigin()
+                                )
+                )
+                .csrf(
+                        csrf ->
+                                csrf.disable()
+                ) // 타임리프, MPA에서는 csrf를 사용함 // REST API 방식에서는 끈다
+                .formLogin(formLogin ->
+                        formLogin.permitAll()
+                );
+        return http.build();
+    }
+}
