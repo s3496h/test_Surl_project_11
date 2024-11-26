88f613db921f08a7559f7254c23f248b3426cd22
s3496h <s3496h@gmail.com>
2024-11-26 13:46:20 +0900

    쿠키

--- src/main/java/com/koreait/Surl_project_11/grobal/rq/Rq.java
+++ src/main/java/com/koreait/Surl_project_11/grobal/rq/Rq.java
@@ -4,12 +4,14 @@ import com.koreait.Surl_project_11.domain.member.entity.Member;
 import com.koreait.Surl_project_11.domain.member.service.MemberService;
 import com.koreait.Surl_project_11.grobal.eceptions.GlobalException;
 import com.koreait.Surl_project_11.standard.util.Ut;
+import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 import org.springframework.web.context.annotation.RequestScope;
 
+import java.util.Arrays;
 @Component
 @RequestScope
 @RequiredArgsConstructor
@@ -17,16 +19,20 @@ public class Rq {
     private final HttpServletRequest req;
     private final HttpServletResponse resp;
     private final MemberService memberService;
+
     private Member member;
 
-//    @Getter
-//    @Setter
-//    private Member member;
+    //   @Getter
+    //   @Setter
+    //  private Member member;
 
     public Member getMember() {
         if (member != null) return member; // 메모리 캐싱
-        String actorUsername = req.getParameter("actorUsername");
-        String actorPassword = req.getParameter("actorPassword");
+
+        String actorUsername = getCookieValue("actorUsername", null);
+        String actorPassword = getCookieValue("actorPassword", null);
+//        String actorUsername = req.getParameter("actorUsername");
+//        String actorPassword = req.getParameter("actorPassword");
 
         if (actorUsername == null || actorPassword == null) {
             String authorization = req.getHeader("Authorization");
@@ -37,6 +43,7 @@ public class Rq {
                 actorPassword = authorizationBits.length == 2 ? authorizationBits[1] : null;
             }
         }
+
 //        if (actorUsername == null) actorUsername = req.getHeader("actorUsername");
 //        if (actorPassword == null) actorPassword = req.getHeader("actorPassword");
 
@@ -47,7 +54,16 @@ public class Rq {
         if (!loginedMember.getPassword().equals(actorPassword)) throw new GlobalException("403-4", "비밀번호 틀림");
 
         member = loginedMember;
+
         return loginedMember;
+    }
+
+    private String getCookieValue(String cookieName, String defaultValue) {
+        return Arrays.stream(req.getCookies()) // 쿠키 배열을 스트림으로 변환
+                .filter(cookie -> cookie.getName().equals(cookieName))// 쿠키의 이름이 매개변수로 쓰이게
+                .findFirst() // 첫 번째 요소
+                .map(Cookie::getValue) // 존재하면 쿠키 값으로 매핑
+                .orElse(defaultValue); // 없으면 기본 값
     }
     public String getCurrentUrlPath() {
         return req.getRequestURI();
