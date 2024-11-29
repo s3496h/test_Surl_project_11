262f68c634654f8e88c1c8773b74c555652590a6
s3496h <s3496h@gmail.com>
2024-11-29 14:17:46 +0900

    일반 Controller 문서 꾸미기

--- src/main/java/com/koreait/Surl_project_11/domain/home/home/controller/HomeContoller.java
+++ src/main/java/com/koreait/Surl_project_11/domain/home/home/controller/HomeContoller.java
@@ -1,5 +1,7 @@
 package com.koreait.Surl_project_11.domain.home.home.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.transaction.annotation.Transactional;
@@ -8,6 +10,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 @Transactional(readOnly = true)
+@Tag(name = "HomeController", description = "홈 컨트롤러")
 public class HomeContoller {
     @Value("${custom.site.name}")
     private String customSiteName;
@@ -16,6 +19,7 @@ public class HomeContoller {
 
     @GetMapping("/")
     @ResponseBody
+    @Operation(summary = "API 메인화면")
     public String ShowMain() {
         return "123Main on " + customSiteName;
     }
